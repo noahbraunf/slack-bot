35d06502909cfaabff438b8ef332c227095a40e6
noahbraunf <noahbraunfeld@gmail.com>
2019-07-02 11:59:02 -0400

    Added function to convert date to integer by removing dashes
    Signed-off-by: VideoGameDesignTSATeam <35616344+tsa1062-904@users.noreply.github.com>

--- Database.py
+++ Database.py
@@ -1,4 +1,4 @@
-from pprint import pprint  # DEBUG IMPORT
+import re
 
 from pymongo import MongoClient
 
@@ -6,6 +6,19 @@ client = MongoClient('localhost', 27017)
 db = client.takendates
 
 users = db.users
+
+
+def parse_dates(dates: tuple):
+    regex = r"(\d\d\d\d)\-(0[0-9]|1[0-2])\-(0[0-9]|1[0-9]|2[0-9]|3[0-1])"
+    regex = re.compile(regex)
+    int_dates: list[int] = []
+    for date in dates:
+        is_date = regex.match(date)
+
+        if is_date:
+            int_dates.append(int(date.replace("-", "")))
+    # pprint(int_dates)
+    return int_dates
 
 
 def update_or_reset_user(user_id: str, name: str, start: int = None, end: int = None):
@@ -23,8 +36,10 @@ def update_or_reset_user(user_id: str, name: str, start: int = None, end: int = 
 
 
 # DEBUG CODE. TODO: REMOVE LATER
-for document in users.find():
-    pprint(document)
+# for document in users.find():
+#     pprint(document)
+# test_dates = ("2019-10-12", "2012-04-33", "2019-22-3", "2019-11-09", "2222-12-31")
+# parse_dates(test_dates)
 
 
 def check_taken(user_id, start, end) -> bool:
