44e2d5785561dbc331cced89dfb0c73a6f9b5d10
noahbraunfeld <35616344+tsa1062-904@users.noreply.github.com>
2019-08-06 13:32:01 -0400

    updated setup.py packages

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 setup(
     name="Slack Scheduler Bot",
     version="0.0.4-ALPHA",
-    packages=['flask', 'slackeventsapi', 'slackclient', 'pymongo'],
+    packages=[
+        'flask', 'slackeventsapi', 'slackclient', 'pymongo', 'python-dotenv',
+        'apscheduler'
+    ],
     long_description=open("readme.md").read(),
 )
